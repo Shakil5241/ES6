6557575ff8bbbc7671ffca74714215f12a0b8cd0
Shakil5241 <sk442664@gmail.com>
2022-03-04 20:30:32 +0600

    ES6

---
+++ Arry.js
@@ -0,0 +1,16 @@
+//Manny types arry function
+
+//function number() {
+//    return 17;}
+//console.log(number());
+
+//let number = (a,b) => {
+    //others thigs here(as your wish)
+//return a+b;}
+//console.log(number(10,5));
+
+//let number =(a,b) => a+b;
+//console.log(number(10,4));
+
+let number =() => 16;
+console.log(number());
