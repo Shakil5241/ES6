a311feb787093152800983918908aa313e039746
Shakil5241 <sk442664@gmail.com>
2022-03-05 11:18:44 +0600

    arry splice

--- Arry slice.js
+++ Arry slice.js
@@ -1,3 +1,4 @@
 var numbers = [1,2,3,4,5,6];
-var result = numbers.slice(-3, 6);
+var result = numbers.slice(-3, 6 );
 console.log(result);
+
---
+++ arry splice.js
@@ -0,0 +1,4 @@
+var numbers = [1,2,3,4,5];
+var result = numbers.splice(-3,1,10,11);
+console.log(numbers);
+console.log(result);
