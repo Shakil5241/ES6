d414c8c1ca8a41f797e3f502971036321e10e198
Shakil5241 <sk442664@gmail.com>
2022-03-06 14:12:15 +0600

    export/import

---
+++ Module import export/exports.js
@@ -0,0 +1,8 @@
+export const pi=3.5;
+export const a=8;
+export default a;
+
+//function import
+export function myFunc(){
+    console.log("i am myFunc()");
+}
---
+++ Module import export/index.js
@@ -0,0 +1,8 @@
+//import *as test from './exports.js';
+//console.log(test.pi);
+
+//default exports
+
+import external,{pi, myFunc} from './exports.js';
+console.log(external,pi,);
+myFunc();
